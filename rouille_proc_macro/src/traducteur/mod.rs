1b524d14ed56b20e18962b53d4c49fd540c51e9e
Ranadeep Biswas <mail@rnbguy.at>
2021-09-13 19:42:08 +0200

    add template

--- rouille_proc_macro/src/traducteur/mod.rs
+++ rouille_proc_macro/src/traducteur/mod.rs
@@ -1,2 +1,12 @@
+/// If you want to add support for your language, copy `template.rs` in `src/traducteur`
+/// and rename it to your language and modify it correctly.
+///
+/// Then add it to `[features]` list in `Cargo.toml`
+/// and define the module here with cfg attribute. Example,
+/// ```ignore
+/// #[cfg(feature = "italiano")]
+/// pub mod italiano
+/// ```
+
 #[cfg(feature = "francais")]
 pub mod francais;
